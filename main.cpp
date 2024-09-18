5487af998254eaef33108461c54f6abe0c0801a4
Ichiro Takahashi <65554077+prostofon@users.noreply.github.com>
2024-09-18 18:37:48 +0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
+#include <cmath>
 
 int main()
 {
-    std::cout<<"Hello World";
-
-    return 0;
+    float r, l, t, fi, angle, speedup, gamma, omega;
+    float v1, v2;
+	r = 0.037;
+	l = 0.164;
+	t = 1.6;
+	fi = 335;
+	angle = M_PI*fi/180;
+	gamma = asin((r*sin(angle))/l);
+	v1 = -(r*angle / t)*(sin(angle + gamma)/cos(gamma));
+	omega = - (r * pow(angle,2)  / pow(t,2))*(cos(angle) + (r*cos(2*angle))/l);
+	v2 = -((r * angle)/t) * (sin(angle) + ((r * (sin(angle)) * (cos(angle))) / l));
+	std::cout<< "" <<v1<<", "<<v2<<", "<<omega;
+	return 0;
 }
